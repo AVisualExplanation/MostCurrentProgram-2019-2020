28a4b3292fd63069a5a43c84ce8ca30b53d4db20
Samuel <samuel.tukua@gmail.com>
2019-11-02 13:45:38 -0400

    affas

--- FrankOtterbot.java
+++ FrankOtterbot.java
@@ -20,65 +20,9 @@ public class FrankOtterbot extends LinearOpMode {
        Right motor is stated as "right_drive", and left motor is "left_drive"*/
      public void runOpMode() throws InterruptedException {
          lowercaseFrank.init(hardwareMap);
-         /* initialize motors */
-
-         // wait for game to start.
          waitForStart();
 
-         // Robot moving
-         // The First number stands for power while the second number stands for time
-
-         DriveForwardTime( DRIVE_POWER, 4000);
-         TurnLeftTime(DRIVE_POWER, 500);
-         DriveForwardTime(DRIVE_POWER, 4000);
-         TurnRightTime(DRIVE_POWER, 500);
-         DriveForwardTime(DRIVE_POWER,4000);
-         StopDriving();
-
      }
-
-/* Establishing the motors*/
-
-
-
-
-    public void DriveForward(double power) {
-        lowercaseFrank.leftDrive.setPower(power);
-        lowercaseFrank.rightDrive.setPower(power);
-    }
-
-    public void DriveForwardTime(double power, long naptime) throws InterruptedException {
-        DriveForward(power);
-        sleep(naptime);
-        DriveForward(0);
-    }
-
-
-    public void TurnLeft(double power) {
-        lowercaseFrank.leftDrive.setPower(-power);
-        lowercaseFrank.rightDrive.setPower(power);
-    }
-
-    public void TurnRight(double power) {
-        TurnLeft(-power);
-    }
-
-    public void TurnLeftTime(double power, long naptime) throws InterruptedException {
-        TurnLeft(power);
-        sleep(naptime);
-        DriveForward(0);
-    }
-
-
-    public void TurnRightTime(double power, long naptime) throws InterruptedException {
-        TurnRight(power);
-        sleep(naptime);
-        DriveForward(0);
-    }
-
-    public void StopDriving() {
-        DriveForward(0);
-    }
 
     private double Rotatetime(double degrees, double power){
         return((degrees)/((1.3646*(Math.pow(10,7))*Math.pow(power,1.47891))-(1.3645*(Math.pow(10,7))*(Math.pow(power,1.47893)))));
