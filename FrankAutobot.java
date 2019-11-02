4be93e4a0d748582dc5115f6ca90f452cb0ed246
Samuel <samuel.tukua@gmail.com>
2019-11-02 11:21:46 -0400

    hkjhkj

--- FrankAutobot.java
+++ FrankAutobot.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.MostCurrentProgram;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -10,92 +11,73 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
  stating this file as autonomous instead of teleop */
 
 
-@Autonomous(name = "FrankAutoBot")
 
-
-public class FrankAutoBot extends OpMode {
+@Autonomous(name = "FrankAutobot", group="Autonomous")
+public class FrankAutobot extends LinearOpMode {
+    private HardwarePushbot lowercaseFrank = new HardwarePushbot();
+    double DRIVE_POWER = .7;
      /* Fetching/stating/getting the motors.
        Right motor is stated as "right_drive", and left motor is "left_drive"*/
+     public void runOpMode() throws InterruptedException {
+         lowercaseFrank.init(hardwareMap);
+         /* initialize motors */
 
+         // wait for game to start.
+         waitForStart();
 
+         // Robot moving
+         // The First number stands for power while the second number stands for time
 
-    public DcMotor getLeft_drive() {
-        return left_drive; }
+         DriveForwardTime( DRIVE_POWER, 4000);
+         TurnLeftTime(DRIVE_POWER, 500);
+         DriveForwardTime(DRIVE_POWER, 4000);
+         TurnRightTime(DRIVE_POWER, 500);
+         DriveForwardTime(DRIVE_POWER,4000);
+         StopDriving();
 
-    public DcMotor getRight_drive() {
-        return right_drive;
-    }
+     }
 
-}
 /* Establishing the motors*/
-     DcMotor left_drive = null;
-     DcMotor right_drive = null;
-
-    @Override public void main() throws InterruptedException {
-        /* initialize motors */
-        left_drive = hardwareMap.dcMotor.get("left_drive");
-        right_drive = hardwareMap.dcMotor.get("right_drive");
-
-        left_drive.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        // wait for game to start.
-
-        wait();
-
-        // Robot moving
-        // The First number stands for power while the second number stands for time
-
-        DriveForwardTime( DRIVE_POWER, 4000);
-        TurnLeftTime(DRIVE_POWER, 500);
-        DriveForwardTime(DRIVE_POWER, 4000);
-        TurnRightTime(DRIVE_POWER, 500);
-        DriveForwardTime(DRIVE_POWER,4000);
-        StopDriving();
 
 
 
-
-    }
-
-    double DRIVE_POWER = 1.0;
 
     public void DriveForward(double power) {
-        left_drive.setPower(power);
-        right_drive.setPower(power);
+        lowercaseFrank.leftDrive.setPower(power);
+        lowercaseFrank.rightDrive.setPower(power);
     }
 
-    public void DriveForwardTime(double power, long time) throws InterruptedException {
+    public void DriveForwardTime(double power, long naptime) throws InterruptedException {
         DriveForward(power);
-        Thread.sleep(time);
-    }
-
-    public void StopDriving() {
+        sleep(naptime);
         DriveForward(0);
     }
-    public void TurnLeft(double power) {
-        left_drive.setPower(-power);
-        right_drive.setPower(power);
-    }
 
-    public void TurnLeftTime(double power, long time) throws InterruptedException {
-        TurnLeft(power);
-        Thread.sleep(time);
+
+    public void TurnLeft(double power) {
+        lowercaseFrank.leftDrive.setPower(-power);
+        lowercaseFrank.rightDrive.setPower(power);
     }
 
     public void TurnRight(double power) {
         TurnLeft(-power);
     }
 
-
-    public void TurnRightTime(double power, long time) throws InterruptedException {
-        TurnRight(power);
-        Thread.sleep(time);
+    public void TurnLeftTime(double power, long naptime) throws InterruptedException {
+        TurnLeft(power);
+        sleep(naptime);
+        DriveForward(0);
     }
 
 
+    public void TurnRightTime(double power, long naptime) throws InterruptedException {
+        TurnRight(power);
+        sleep(naptime);
+        DriveForward(0);
+    }
 
-
-
-
+    public void StopDriving() {
+        DriveForward(0);
+    }
 
 }
